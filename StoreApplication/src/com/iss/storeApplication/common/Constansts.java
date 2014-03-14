0a05cc23581cbdb70eeb03640b8ed648c373e104
milan <milandashara@gmail.com>
2014-03-14 14:17:48 +0800

    Save Object to File. Generic Implementation

--- StoreApplication/src/com/iss/storeApplication/common/Constansts.java
+++ StoreApplication/src/com/iss/storeApplication/common/Constansts.java
@@ -5,4 +5,8 @@ public interface Constansts {
 	public static final String PRODUCT_NAME="Product Name";
 	public static final String SUCCESS = "SUCCESS";
 	public static final String ALWAYS = "Always";
+	public static final Object DELIMITER = ",";
+	public static final String FILE_DIR="";
+	public static final String EXTENSION="dat";
+	public static final String SEPERATOR="/";
 }
--- StoreApplication/src/com/iss/storeApplication/dao/CommonDao.java
+++ StoreApplication/src/com/iss/storeApplication/dao/CommonDao.java
@@ -1,50 +1,44 @@
 package com.iss.storeApplication.dao;
 
-import java.util.List;
 import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Serializable;
-import java.io.Writer;
 import java.lang.reflect.Field;
 
-import com.sun.org.apache.bcel.internal.generic.Type;
+import com.iss.storeApplication.common.Constansts;
 
-import sun.org.mozilla.javascript.internal.json.JsonParser;
-import sun.reflect.FieldInfo;
+public  class CommonDao {
 
-public class CommonDao {
-
-	public static <T, Z> boolean save(T Dao, Z filePath) {
-		String fpath = filePath.toString();
-			try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fpath, true)))) {
-			    out.println(CommonDao.toCsv(Dao));
+	public static <T> boolean save(T Dao, String fileName) {
+		
+			try {
+				File file=new File(Constansts.FILE_DIR,fileName);
+				if(!file.exists())
+				{
+					file.createNewFile();
+				}
+				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(Constansts.FILE_DIR+Constansts.SEPERATOR+fileName, true)));
+			    out.println(CommonDao.objectToCsv(Dao));
 			    out.close();
 			}
 			catch (IOException e) {
 				System.err.println("File writing error while writing : "
-						+ Dao.getClass() + " to path : " + fpath);
+						+ Dao.getClass() + " to path : " + fileName);
 				return false;
 			}			
 			return true;
 	}
 
-	private static String toCsv(Object o) throws IOException {
+	private static String objectToCsv(Object o) throws IOException {
 		StringBuffer sb = new StringBuffer();
 		Field[] fields = o.getClass().getDeclaredFields();
 		for (int i = 0; i < fields.length; i++) {
 			try {
 				fields[i].setAccessible(true);
-				String FieldKey = fields[i].getName();
-				Object Fieldvalue;
-				Fieldvalue = fields[i].get(o);
-				sb.append(Fieldvalue).append(',');
+				Object fieldValue = fields[i].get(o);
+				sb.append(fieldValue).append(Constansts.DELIMITER);
 			} catch (IllegalArgumentException e) {
 				e.printStackTrace();
 			} catch (SecurityException e) {
@@ -53,6 +47,7 @@ public class CommonDao {
 				e.printStackTrace();
 			}
 		}
+		//remove last deliminator
 		sb.delete(sb.length()-1, sb.length());
 		return sb.toString();
 	}
