4de41c356e6ff81eeaf07e0e8b4e9c7a286f4d74
luke8like <luke8like@gmail.com>
2014-03-15 16:13:52 +0800

    change to categoryView

--- StoreApplication/src/com/iss/storeApplication/view/CategoryView.java
+++ StoreApplication/src/com/iss/storeApplication/view/CategoryView.java
@@ -66,7 +66,7 @@ public class CategoryView extends JPanel {
 						    String categoryCode = field1.getText();  
 						    String categoryName = field2.getText();  
 						    System.out.println("categoryCode:"+categoryCode);
-						    System.out.println("categoryCode:"+categoryName);
+						    System.out.println("categoryName:"+categoryName);
 						} 
 
 			}
