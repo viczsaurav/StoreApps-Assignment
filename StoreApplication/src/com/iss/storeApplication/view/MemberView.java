deb7d772c41f0fcfa3ad3411af371dcaa54ef9ea
Siva <Siva@192.168.2.6>
2014-04-01 17:26:50 -0700

    Memberview updated.

--- StoreApplication/src/com/iss/storeApplication/view/MemberView.java
+++ StoreApplication/src/com/iss/storeApplication/view/MemberView.java
@@ -1,44 +1,74 @@
 package com.iss.storeApplication.view;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
-import javax.swing.table.TableModel;
 
 import com.iss.storeApplication.common.Constants;
 import com.iss.storeApplication.common.Utility;
 
+
+
 /**
  * 
- * @author milan
+ * @author Siva
  *
  */
 public class MemberView extends JPanel{
-
+	/*
 	public MemberView() {
 		add(new Button("MemberView"));
 	}
-	/*
+	*/
 	private MainView mainView;
+	
+	public MemberView(MainView mainView) {
+		super(new BorderLayout());
+		this.mainView = mainView;
+		/*addMemberBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				addBtnClicked(event);
+
+			}
+		});*/
+		editMemberBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+			//	editBtnClicked(event);
+
+			}
+		});
+		
+		//init top panel
+		initTopPanel();
+		//init center panel
+		initCenterPanel();	
+	}
 	//Top and center panels
 	private JPanel topPanel = new JPanel();
 	private JPanel centerPanel = new JPanel();
-	
+	private final JPanel addMemberPanel = new JPanel();	
 	// add Edit Delete
 			private JButton deleteMemberBtn = new JButton("Delete Member");
 			private JButton addMemberBtn = new JButton(
 					Utility.getPropertyValue(Constants.addMemberBtn));
 			private JButton editMemberBtn = new JButton(
 					Utility.getPropertyValue(Constants.editMemberBtn));
-			
+	
 	//Member Labels
 	private final JLabel memberNameLabel = new JLabel(
 			Utility.getPropertyValue(Constants.memberName));
@@ -80,9 +110,60 @@ public class MemberView extends JPanel{
 	   topPanel.add(addMemberBtn, BorderLayout.NORTH);
 	   topPanel.add(editMemberBtn, BorderLayout.NORTH);
 	   topPanel.add(deleteMemberBtn, BorderLayout.NORTH);
-	   
+	   addMemberBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				addBtnClicked(event);
+
+			}
+		});
+	}
+
+	
+	private void initAddMemberDialog() {
+		addMemberPanel.setLayout(new GridBagLayout());
+		//final GridBagConstraints c = new GridBagConstraints();
+
+		
+
+		memberNameField.setColumns(20);
+		memberNameField.setText("");
+		memberIdField.setColumns(20);
+		memberIdField.setText("");
+		loyalityField.setColumns(20);
+		loyalityField.setText("");
+	}
+	
+	
+	protected void addBtnClicked(ActionEvent event) {
+
+		showAddMemberDialog();
+	}
+
+	private void showAddMemberDialog() {
+		addMemberPanel.setLayout(new GridBagLayout());                
+		int result = JOptionPane.showConfirmDialog(mainView, addMemberPanel,
+						Utility.getPropertyValue(Constants.addMemberBtn),
+						JOptionPane.OK_CANCEL_OPTION);
+		
+		                if (result == JOptionPane.OK_OPTION) {
+		                	
+		                }
+	
+	
 	}
 
 
-*/
+
+	private void initGUI() {
+		try {
+			{
+				this.setPreferredSize(new java.awt.Dimension(0, 36));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
